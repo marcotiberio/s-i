128dd172e2d79a023012b59461198deac997cd03
Marco Tiberio <45846191+marcotiberio@users.noreply.github.com>
2022-02-05 16:48:05 +0100

    video/image in articles meta

--- inc/fieldGroups/articleComponents.php
+++ inc/fieldGroups/articleComponents.php
@@ -12,13 +12,20 @@ add_action('Flynt/afterRegisterComponents', function () {
         'position' => 'acf_after_title',
         'fields' => [
             [
+                'label' => __('Info', 'flynt'),
+                'name' => 'infoTab',
+                'type' => 'tab',
+                'placement' => 'top',
+                'endpoint' => 0
+            ],
+            [
                 'label' => __('Intro', 'flynt'),
                 'name' => 'introArticle',
                 'type' => 'textarea',
                 'wrapper' => [
                     'width' => '100',
                 ]
-                ],
+            ],
             [
                 'label' => __('Date', 'flynt'),
                 'name' => 'datePost',
@@ -27,7 +34,7 @@ add_action('Flynt/afterRegisterComponents', function () {
                 'wrapper' => [
                     'width' => '50',
                 ]
-                ],
+            ],
             [
                 'label' => __('Author', 'flynt'),
                 'name' => 'authorArticle',
@@ -35,7 +42,35 @@ add_action('Flynt/afterRegisterComponents', function () {
                 'wrapper' => [
                     'width' => '50',
                 ]
-            ]
+            ],
+            [
+                'label' => __('Media', 'flynt'),
+                'name' => 'mediaTab',
+                'type' => 'tab',
+                'placement' => 'top',
+                'endpoint' => 0
+            ],
+            [
+                'label' => __('Poster Image', 'flynt'),
+                'name' => 'posterImage',
+                'type' => 'image',
+                'preview_size' => 'medium',
+                'mime_types' => 'jpg,jpeg,png',
+                'instructions' => __('Recommended Size: Min-Width 1920px; Min-Height: 1080px; Image-Format: JPG, PNG. Important: Aspect ratio needs to match the embedded video aspect ratio.', 'flynt'),
+                'required' => 0,
+                'wrapper' => [
+                    'width' => '50',
+                ],
+            ],
+            [
+                'label' => __('Video/Image', 'flynt'),
+                'name' => 'videoEmbedLink',
+                'type' => 'textarea',
+                'required' => 0,
+                'wrapper' => [
+                    'width' => '50',
+                ],
+            ],
         ],
         'location' => [
             [
