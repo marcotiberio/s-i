08aa0cb9e39a79630d3f7e3b99295f7ac211982d
Marco Tiberio <45846191+marcotiberio@users.noreply.github.com>
2022-02-21 13:52:22 +0100

    test for non-cropped images in sliders

--- inc/fieldGroups/artistComponents.php
+++ inc/fieldGroups/artistComponents.php
@@ -4,6 +4,66 @@ use ACFComposer\ACFComposer;
 use Flynt\Components;
 
 add_action('Flynt/afterRegisterComponents', function () {
+    // ACFComposer::registerFieldGroup([
+    //     'name' => 'artistMeta',
+    //     'title' => 'Artist Meta',
+    //     'style' => '',
+    //     'menu_order' => 1,
+    //     'position' => 'acf_after_title',
+    //     'fields' => [
+    //         [
+    //             'label' => __('Artist Bio', 'flynt'),
+    //             'name' => 'bioTab',
+    //             'type' => 'tab',
+    //             'placement' => 'top',
+    //             'endpoint' => 0
+    //         ],
+    //         [
+    //             'label' => __('Bio', 'flynt'),
+    //             'name' => 'bio',
+    //             'type' => 'wysiwyg',
+    //             'tabs' => 'visual',
+    //             'wrapper' => [
+    //                 'width' => '100',
+    //             ]
+    //         ],
+    //         [
+    //             'label' => __('Socials', 'flynt'),
+    //             'name' => 'socialTab',
+    //             'type' => 'tab',
+    //             'placement' => 'top',
+    //             'endpoint' => 0
+    //         ],
+    //         [
+    //             'label' => __('Socials', 'flynt'),
+    //             'name' => 'repeaterPanels',
+    //             'type' => 'repeater',
+    //             'layout' => 'row',
+    //             'min' => 1,
+    //             'button_label' => __('Add Social Link', 'flynt'),
+    //             'sub_fields' => [
+    //                 [
+    //                     'label' => __('Panel Link', 'flynt'),
+    //                     'name' => 'panelLink',
+    //                     'type' => 'link',
+    //                     'return_format' => 'array',
+    //                     'wrapper' => [
+    //                         'width' => '100',
+    //                     ],
+    //                 ],
+    //             ],
+    //         ],
+    //     ],
+    //     'location' => [
+    //         [
+    //             [
+    //                 'param' => 'post_type',
+    //                 'operator' => '==',
+    //                 'value' => 'artist',
+    //             ],
+    //         ],
+    //     ],
+    // ]);
     ACFComposer::registerFieldGroup([
         'name' => 'artistComponents',
         'title' => 'Artist Components',
